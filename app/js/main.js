c5b6596ba2b038ac040934928cdd6511a053df70
VSprod <hbears984@gmail.com>
2020-05-22 00:40:54 +0300

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,25 @@
 $(function(){
 
+
+    $('.rate-star').rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px",
+        spacing: "3px"
+
+    });
+
+
+    $('.product-slider__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        
+    });
+
+
     var mixer = mixitup('.product__inner-box');
 
-
+   
 });
